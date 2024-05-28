7765a94060466a70651803dfbbb9b5f20e93b903
Raddames <raddamestonui48@gmail.com>
2024-05-28 13:59:16 +0300

    done

---
+++ .github/lib/student.py
@@ -0,0 +1,17 @@
+from user import User
+
+class Student(User):
+    def __init__(self, name, grade):
+        print("Student.__init__ called.")
+        super().__init__(name)
+        self.grade = grade
+
+    def log_in(self):
+        super().log_in()
+        self.in_class = True
+
+oneil = Student("O'neil", 10)
+
+oneil.__dict__
+
+
